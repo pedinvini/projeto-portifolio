fe96f2fdfd6f7c56de7c34a9da450de9426dc3fb
Pedro Vinicius <92653352+pedinvini@users.noreply.github.com>
2025-09-01 11:43:47 -0300

    projeto atualizado com o scroll

--- projeto-portifolio/assets/js/form.js
+++ projeto-portifolio/assets/js/form.js
@@ -2,6 +2,8 @@ const form = document.getElementById("form-contato");
 const textName = document.getElementById("nome");
 const textEmail = document.getElementById("email");
 const textMessage = document.getElementById("mensagem");
+const btnTopo = document.getElementById("voltar-topo");
+
 
 function mensageERROR(mensagem) {
     const containerMensageERROR = document.querySelector(".mensageERROR");
@@ -9,35 +11,25 @@ function mensageERROR(mensagem) {
     containerMensageERROR.innerHTML = mensagem;
 
 }
-
 function validateName(name) {
   const arrayName = name.trim().split(" ").filter(part => part !== "");
 
-  // First check: make sure there's at least a first and last name
   if (arrayName.length < 2) {
-    console.log("Digite o nome e sobrenome: ");
     mensageERROR("Por favor, insira um nome e sobrenome válidos");
-    return false; // Return false immediately
+    return false;
   }
-
-  // Second check: check the length of each part of the name
   for (let i = 0; i < arrayName.length; i++) {
-    const part = arrayName[i]; // Use a new variable name
+    const part = arrayName[i];
     if (part.length < 2) {
       mensageERROR("Cada parte do nome deve ter pelo menos 2 caracteres.");
-      console.log("Cada parte do nome deve ter pelo menos 2 caracteres.");
-      return false; // Stop and return false if any part is invalid
+      return false;
     }
   }
-
-  // If the loop finishes without returning false, the name is valid
   console.log(`Nome válido: ${name}`);
   return true;
 }
-
 form.addEventListener('submit', function (e) {
   e.preventDefault();
-
   const isNameValid = validateName(textName.value);
 
   if (isNameValid) {
@@ -51,4 +43,19 @@ form.addEventListener('submit', function (e) {
     containerMensageERROR.style.display = 'none';
     form.reset();
   }
+});
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    btnTopo.classList.add("show");
+  } else {
+    btnTopo.classList.remove("show");
+  }
+});
+
+btnTopo.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
 });
